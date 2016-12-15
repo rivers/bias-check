0a31f7d7bcad8c330b6bc505c53bfd87e096a2f9
Heather Rivers <git@heatherrivers.com>
2016-12-14 22:25:42 -0800

    Initial import

---
+++ README.md
@@ -0,0 +1,9 @@
+# Bias Check
+
+A Google Chrome extension that shows media bias in your Facebook news feed
+
+### Packaging
+
+```sh
+bin/build
+```
---
+++ src/content.js
@@ -0,0 +1,174 @@
+var BC = {};
+
+BC.domainMap = {
+  "bipartisanreport.com": ["Bipartisan Report", 4.5, 1.5],
+  "davidwolfe.com": ["David Wolfe", 1, 4.5],
+  "addictinginfo.org": ["Addicting Info", 1, 4.5],
+  "occupydemocrats.com": ["Occupy Democrats", 2, 4.5],
+  "usuncut.com": ["U.S. Uncut", 2, 5],
+  "huffingtonpost.com": ["Huffington Post", 2, 2.5],
+  "vox.com": ["Vox", 3, 2],
+  "slate.com": ["Slate", 3, 1.5],
+  "theatlantic.com": ["The Atlantic", 3, 1],
+  "theguardian.com": ["The Guardian", 3.5, 1.5],
+  "npr.org": ["NPR", 4, 2],
+  "wsj.com": ["WSJ", 4.5, 1.5],
+  "msnbc.com": ["MSNBC", 3, 2],
+  "economist.com": ["Economist", 4.5, 1],
+  "bbc.com": ["BBC", 4, 2],
+  "washingtonpost.com": ["Washington Post", 4, 2.5],
+  "nytimes.com": ["NY Times", 4, 2.5],
+  "nbcnews.com": ["NBC News", 4, 3],
+  "abcnews.go.com": ["ABC News", 4, 3],
+  "ap.org": ["AP", 4, 3],
+  "reuters.com": ["Reuters", 4, 3],
+  "usatoday.com": ["USA Today", 4, 4],
+  "cnn.com": ["CNN", 4, 4.5],
+  "thehill.com": ["The Hill", 5, 2],
+  "foxnews.com": ["Fox News", 5.5, 3],
+  "redstate.com": ["RedState", 6, 4.5],
+  "theblaze.com": ["The Blaze", 6, 5],
+  "dailycaller.com": ["Daily Caller", 7, 4.5],
+  "breitbart.com": ["Breitbart", 7, 4.5],
+  "infowars.com": ["InfoWars", 7, 5],
+  "thefiscaltimes.com": ["The Fiscal Times", 5, 1.5]
+};
+
+BC.qualityMap = {
+  "1": "Complex",
+  "1.5": "Complex/Analytical",
+  "2": "Analytical",
+  "2.5": "Analytical/Meets High Standards",
+  "3": "Meets High Standards",
+  "3.5": "Meets High Standards/Basic AF",
+  "4": "Basic AF",
+  "4.5": "Basic AF/Sensational clickbait",
+  "5": "Sensational clickbait"
+};
+
+BC.biasMap = {
+  "1": "Liberal utter garbage/conspiracy theories",
+  "1.5": "Liberal utter garbage/hyper-partisan liberal",
+  "2": "Hyper-partisan liberal (questionable journalistic value)",
+  "2.5": "Skews liberal/hyper-partisan liberal",
+  "3": "Skews liberal (but still reputable)",
+  "3.5": "Mainstream/skews liberal",
+  "4": "Mainstream (minimal partisan bias)",
+  "4.5": "Mainstream/skews conservative",
+  "5": "Skews conservative (but still reputable)",
+  "5.5": "Skews conservative/hyper-partisan conservative",
+  "6": "Hyper-partisan conservative (questionable journalistic value)",
+  "6.5": "Conservative utter garbage/hyper-partisan conservative",
+  "7": "Conservative utter garbage/conspiracy theories"
+};
+
+BC.storyMap = {};
+
+BC.scanning = false;
+
+BC.domainFromUrl = function(url) {
+  var a = document.createElement("a");
+  a.href = url;
+  return psl.parse(a.hostname).domain;
+};
+
+BC.queryParameter = function(name, url) {
+  name = name.replace(/[\[\]]/g, "\\$&");
+
+  var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)")
+    , results = regex.exec(url);
+
+  if (!results) { return null; }
+  if (!results[2]) { return ""; }
+
+  return decodeURIComponent(results[2].replace(/\+/g, " "));
+};
+
+BC.generateGuid = function() {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function(c) {
+    var r = Math.random()*16|0, v = c == "x" ? r : (r&0x3|0x8);
+    return v.toString(16);
+  });
+};
+
+BC.getMode = function(array) {
+  if (array.length === 0) { return null; }
+  var modeMap = {};
+  var maxEl = array[0], maxCount = 1;
+  for (var i = 0; i < array.length; i++) {
+    var el = array[i];
+    if (modeMap[el] == null) {
+      modeMap[el] = 1;
+    } else {
+      modeMap[el]++;
+    }
+    if (modeMap[el] > maxCount) {
+      maxEl = el;
+      maxCount = modeMap[el];
+    }
+  }
+  return maxEl;
+};
+
+BC.buildInfoHtml = function(domain, info) {
+  var name = info[0]
+    , quality = info[1]
+    , bias = info[2];
+
+  var content = "<div class=\"bias-check-container\">";
+  content += "<img class=\"bias-check-icon\" src=\"";
+  content += chrome.extension.getURL('images/icon-16.png');
+  content += "\" />";
+  content += "<span class=\"bias-check-label\">Bias Check</span>";
+  content += domain + ': ';
+  content += BC.qualityMap[quality.toString()];
+  content += ", " + BC.biasMap[bias.toString()];
+  content += "</div>";
+
+  return content;
+};
+
+BC.scan = function() {
+  if (BC.scanning) { return; }
+
+  BC.scanning = true;
+
+  $(".userContentWrapper").each(function() {
+    if (this.dataset.checkBiasId) { return true; }
+
+    var $this = this;
+    if ($this.parents(".userContentWrapper").length) { return true; }
+
+    var guid = BC.generateGuid();
+    this.dataset.checkBiasId = guid;
+
+    var domains = [];
+
+    $this.find("a").each(function() {
+      if (this.hostname !== "l.facebook.com") { return true; }
+      var target = BC.queryParameter("u", this.href);
+      domains.push(BC.domainFromUrl(target));
+    });
+
+    var domain = BC.getMode(domains);
+
+    BC.storyMap[guid] = domain;
+
+    if (domain === null) { return true; }
+
+    var info = BC.domainMap[domain];
+
+    if (info === undefined) { return true; }
+
+    $this.prepend($(BC.buildInfoHtml(domain, info)));
+  });
+
+  BC.scanning = false;
+};
+
+BC.init = function() {
+  document.addEventListener("scroll", BC.scan);
+  BC.scan();
+};
+
+BC.init();
